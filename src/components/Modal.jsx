bd14dd2c1ab5cfff7a52716e3f0137dc3c04a30f
ervanervan <ervanervan040@gmail.com>
2024-07-22 21:39:32 +0700

    update: modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({ children, isOpen, onClose }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-white rounded-lg overflow-hidden shadow-xl max-w-md w-full">
-        <div className="p-4 flex justify-end">
+        <div className="px-4 pt-4 flex justify-end">
           <button
             onClick={onClose}
             className="text-gray-500 hover:text-gray-700"
