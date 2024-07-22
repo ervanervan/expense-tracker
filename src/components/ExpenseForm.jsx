5d604a818a9deb6d3c30d646dc30d34381912891
ervanervan <ervanervan040@gmail.com>
2024-07-22 21:51:13 +0700

    update: expenseForm component

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -65,12 +65,14 @@ const ExpenseForm = ({ onAddExpense }) => {
           className="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
       </div>
-      <button
-        type="submit"
-        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition duration-200 ease-in-out"
-      >
-        Add Expense
-      </button>
+      <div className="flex justify-end">
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-lg transition duration-200 ease-in-out"
+        >
+          Add Expense
+        </button>
+      </div>
     </form>
   );
 };
