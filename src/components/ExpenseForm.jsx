c6157ebf740c84ee4512ffac9912a26b6cec73e5
ervanervan <ervanervan040@gmail.com>
2024-08-07 15:40:11 +0700

    add: feature update expenses

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ExpenseForm = ({ onAddExpense, onUpdateExpense, currentExpense }) => {
   const [name, setName] = useState("");
